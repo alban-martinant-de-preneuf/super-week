dba0fc1e1018273763820b198726f1c427431920
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-05-02 20:03:38 +0200

    register form inital commit

---
+++ src/View/register.php
@@ -0,0 +1,8 @@
+<form action="">
+    <input type="text" name="firtname" id="firstname">
+    <label for="firstname">Firstname</label>
+    <input type="text" name="lastname" id="lastname">
+    <label for="lastname">Lastname</label>
+    <input type="text" name=email" id="email">
+    <label for="email">Email</label>
+</form>
