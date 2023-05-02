52e5aca2b02ca10e02f295bcf5e1675a8a85b4ba
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-05-02 14:16:03 +0200

    autoloader ok

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit667ae47bf0380930bf852b2bacfc0d32
 {
+    public static $prefixLengthsPsr4 = array (
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static $classMap = array (
         'AltoRouter' => __DIR__ . '/..' . '/altorouter/altorouter/AltoRouter.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
@@ -14,6 +28,8 @@ class ComposerStaticInit667ae47bf0380930bf852b2bacfc0d32
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit667ae47bf0380930bf852b2bacfc0d32::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit667ae47bf0380930bf852b2bacfc0d32::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit667ae47bf0380930bf852b2bacfc0d32::$classMap;
 
         }, null, ClassLoader::class);
