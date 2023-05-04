da5642d451b521e62fae6762cd183bf797ffedd2
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-05-04 18:37:57 +0200

    add buttons on home page

--- src/View/login.php
+++ src/View/login.php
@@ -16,7 +16,7 @@
         <input type="text" name="email" id="email">
         <label for="password">Password</label>
         <input type="password" name="password" id="password">
-        <input type="submit" name="submit">
+        <input type="submit" name="submit" value="Submit">
     </form>
 
 </body>
